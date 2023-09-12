4682ef09719d84259f6858868d7ea1ab415b79d7
Harsh Patel <harshnpatel9@gmail.com>
2023-09-12 16:17:11 +0530

    fix routing

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,9 +1,32 @@
 import { Router } from 'express';
-
+import { fetchResponse, generateResponse } from './ml-model';
+function delay(milliseconds: number): Promise<void> {
+  return new Promise<void>(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, milliseconds);
+  });
+}
 export default (): Router => {
   const app = Router();
 
-  //TODO: add routes here...
+  app.post('/generate', async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader?.split(' ')[1];
+    const body = req.body.msg;
+
+    if (!token) {
+      return res.status(401).json({ message: 'No token provided' });
+    }
+
+    if (!body) {
+      return res.status(400).json({ message: 'No body provided' });
+    }
+    const { process_id } = await generateResponse(token, body);
+    delay(3000);
+    const response = await fetchResponse(token, process_id);
+    return res.json({ message: response });
+  });
 
   return app;
 };
